68d5b22c8d4ba477ec972eaa883f8ebbd9735b97
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2021-05-17 22:37:48 -0500

    mongo id validator pipe

--- src/utils/generc-querys.service.ts
+++ src/utils/generc-querys.service.ts
@@ -1,9 +1,18 @@
+import { NotFoundException } from '@nestjs/common';
 import { Model } from 'mongoose';
 
 export default class GenericQuerysService<T> {
   constructor(protected model: Model<T>) {}
 
-  findAll() {
-    return this.model.find().exec();
+  async findOneById(id: string) {
+    const document = await this.model.findById(id).exec();
+    if (!document) {
+      throw new NotFoundException();
+    }
+    return document;
+  }
+
+  async findAll() {
+    return await this.model.find().exec();
   }
 }
