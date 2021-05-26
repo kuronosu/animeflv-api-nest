0b0a49b99acf38f1e372d6520fc4af5a252fc2cc
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2021-05-25 22:36:13 -0500

    ✨ states endpoints

--- src/animes/controllers/states.controller.ts
+++ src/animes/controllers/states.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateStateDto, UpdateStateDto } from '../dtos/generic.dto';
+import { StatesService } from '../services/states.service';
 
 @Controller('states')
-export class StatesController {}
+export class StatesController {
+  constructor(private service: StatesService) {}
+
+  @Get()
+  getAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.service.findOneById(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateStateDto) {
+    return this.service.create(payload);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateStateDto,
+  ) {
+    return this.service.update(id, payload);
+  }
+}
--- src/animes/services/states.service.ts
+++ src/animes/services/states.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { GenericQuerysService } from 'src/common/generc-querys.service';
+import { CreateStateDto, UpdateStateDto } from '../dtos/generic.dto';
+import { State } from '../entities';
 
 @Injectable()
-export class StatesService {}
+export class StatesService extends GenericQuerysService<
+  State,
+  CreateStateDto,
+  UpdateStateDto
+> {
+  constructor(@InjectModel(State.name) model: Model<State>) {
+    super(model);
+  }
+}
